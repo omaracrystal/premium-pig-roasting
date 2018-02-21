cd0effdc27749d8b1169eb87e9670cd8558038a9
Crystal O'Mara <Crystal.O'Mara@starz.com>
2018-02-20 23:33:07 -0700

    updated home page

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,11 +1,14 @@
 import { Component, OnInit } from '@angular/core';
 
+const LOGO_PIG = '/../../assets/images/logo-pig.svg';
+
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
+  logoPig = LOGO_PIG;
 
   constructor() { }
 
