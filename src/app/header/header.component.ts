a4b931f08c69bfef530904f4d8ac1f08ac553d90
Crystal O'Mara <Crystal.O'Mara@starz.com>
2018-02-20 21:57:37 -0700

    added logo and favicon added fontawesome and added how to steps

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,11 +1,14 @@
 import { Component, OnInit } from '@angular/core';
 
+const LOGO = '/../../assets/images/logo.svg';
+
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent implements OnInit {
+  logo = LOGO;
 
   constructor() { }
 
