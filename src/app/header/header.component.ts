a68ba0a6dd21c6addd521429d4b79724c3ae3982
Crystal O'Mara <Crystal.O'Mara@starz.com>
2018-02-21 11:10:27 -0700

    added mobile menu

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 const LOGO = '/../../assets/images/logo.svg';
+const LOGO_MOBILE = '/../../assets/images/mobile-logo.svg';
 
 @Component({
   selector: 'app-header',
@@ -9,6 +10,7 @@ const LOGO = '/../../assets/images/logo.svg';
 })
 export class HeaderComponent implements OnInit {
   logo = LOGO;
+  mobileLogo = LOGO_MOBILE;
 
   constructor() { }
 
