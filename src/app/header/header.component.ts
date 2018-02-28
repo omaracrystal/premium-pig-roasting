a3cc3d72a51612fd2fc60a20bed71cfb394634ad
Crystal O'Mara <Crystal.O'Mara@starz.com>
2018-02-28 12:33:54 -0700

    added active class to location hash values for on load

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -13,17 +13,40 @@ export class HeaderComponent implements OnInit {
   logo = LOGO;
   mobileLogo = LOGO_MOBILE;
   bottomNav: boolean = true;
+  // active menu items
+  activeMenuItemServices: boolean = false;
+  activeMenuItemAbout: boolean = false;
+  activeMenuItemHow: boolean = false;
+  activeMenuItemContact: boolean = false;
+  activeMenuItemTestimonials: boolean = false;
 
   constructor() { }
 
   ngOnInit() {
+    if (location.hash === '#services') {
+      this.activeMenuItemServices = true;
+    }
+    if (location.hash === '#about') {
+      this.activeMenuItemAbout = true;
+    }
+    if (location.hash === '#how') {
+      this.activeMenuItemHow = true;
+    }
+    if (location.hash === '#contact') {
+      this.activeMenuItemContact = true;
+    }
+    if (location.hash === '#testimonials') {
+      this.activeMenuItemTestimonials = true;
+    }
   }
 
   goToRoute(id) {
     document.getElementById(id).scrollIntoView();
-  }
-
-  toggleActive() {
-
+    location.replace('#'+ id);
+    this.activeMenuItemServices = false;
+    this.activeMenuItemAbout = false;
+    this.activeMenuItemHow = false;
+    this.activeMenuItemContact = false;
+    this.activeMenuItemTestimonials = false;
   }
 }
